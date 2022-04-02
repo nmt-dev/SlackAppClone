49c648529a9654c98ddc26626bd702bb4e4ce828
Nazareno Mateo <nazamateo@gmail.com>
2022-04-02 22:19:42 +0800

    addtopbar layouts

--- src/Components/SideBar/Channels.jsx
+++ src/Components/SideBar/Channels.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
+import styles from "./Channels.module.scss";
 
 function Channels() {
-  let channels = ["Avion", "Lasal", "Ateneo"];
-  const [channelsArray, SetChannelsArray] = useState([channels]);
+  const channels = ["Avion", "Lasal", "Ateneo"];
+  const [channelsArray, SetChannelsArray] = useState([...channels]);
 
   return (
     <>
-      <ul>
-        {channelsArray.map((channel) => (
-          <li>{channel}</li>
-        ))}
-      </ul>
+      <div className={styles.contain}>
+        <ul className={styles.list}>
+          {channelsArray.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
