fd8f3d09ab005ad1c04a0ae428a0c93fe72e629d
Nazareno Mateo <nazamateo@gmail.com>
2022-04-05 16:38:37 +0800

    add testAPI and sign up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import LandingPage from "../src/Pages/LandingPage";
+import SignUp from "./Components/Signup/Signup";
 import SlackPage from "./Pages/SlackPage";
+import TestAPI from "./Utils/TestAPI";
 
 function App() {
   return (
     <>
       <LandingPage />
+      <SignUp />
+      <SlackPage />
     </>
   );
 }
