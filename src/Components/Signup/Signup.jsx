fc0a34406c619ff2c4516c2df4b21944e1c01b4c
Nazareno Mateo <nazamateo@gmail.com>
2022-04-06 22:30:14 +0800

    done user registration functions using axios

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,51 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Signup/Signup.module.scss";
 import Input from "../General/Input";
 import Btn from "../General/Button";
 import Img from "../General/Img";
 import team1 from "../../Assets/Images/team1.png";
 import TopBarLanding from "../TopBar/Topbarlanding";
+import { useNavigate } from "react-router-dom";
+import API from "../../Utils/API";
+import Success from "../Success";
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirm, setPassword_Confirm] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [errorStyle, setErrorStyle] = useState(styles.inputfield);
+  const [errorStyleIcon, setErrorStyleIcon] = useState(styles.icons);
+  const [showPassword, setShowPassword] = useState("password");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    if (password === password_confirm) {
+      const response = await API.post("/auth", {
+        email,
+        password,
+        password_confirm,
+      }).catch((error) => {
+        console.log(error.response.data.errors.full_messages);
+        setPasswordError(error.response.data.errors.full_messages);
+      });
+      console.log(response);
+      if (response !== undefined) {
+        setIsLoading(true);
+      }
+    } else {
+      setPasswordError("Password does not match");
+      setErrorStyle(styles.inputfielderror);
+      setErrorStyleIcon(styles.iconserror);
+    }
+  };
+  function Home() {
+    navigate("/");
+  }
+  function logIn() {
+    navigate("/LogIn");
+  }
+  function showMyPassword() {
+    setShowPassword("text");
+  }
+  function hideMyPassword() {
+    setShowPassword("password");
+  }
+  function resetInputStyle() {
+    setErrorStyle(styles.inputfield);
+    setErrorStyleIcon(styles.icons);
+    setPasswordError("");
+  }
+
   return (
     <>
+      {isLoading && <Success />}
       <TopBarLanding />
       <div className={styles.container}>
         <div className={styles.signupcontainer}>
           <h1>
-            Sign Up to <span className={styles.mat}>MatSlack</span>
+            Sign Up to{" "}
+            <span onClick={Home} className={styles.mat}>
+              MatSlack
+            </span>
           </h1>
-          <h6>Already a member? Log In</h6>
-          <form>
+          <form onSubmit={HandleSubmit}>
             <div className={styles.inputicon}>
               <Input
                 className={styles.inputfield}
                 type={"email"}
                 placeholder={"Email"}
-                error={""}
-                errorclassname={styles.error}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                autoComplete="off"
               />{" "}
               <i id={styles.icons} class="las la-envelope"></i>
             </div>
             <div className={styles.inputicon}>
               <Input
-                className={styles.inputfield}
-                type={"password"}
+                className={errorStyle}
+                type={showPassword}
                 placeholder={"Password"}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                onClick={resetInputStyle}
               />
-              <i id={styles.icons} class="las la-lock"></i>
+              <i
+                onMouseOver={showMyPassword}
+                onMouseLeave={hideMyPassword}
+                id={errorStyleIcon}
+                class="las la-lock"
+              ></i>
             </div>
             <div className={styles.inputicon}>
               <Input
-                className={styles.inputfield}
-                type={"password"}
+                className={errorStyle}
+                type={showPassword}
                 placeholder={"Confirm Password"}
-                error={""}
-                errorclassname={styles.error}
+                onChange={(e) => setPassword_Confirm(e.target.value)}
+                value={password_confirm}
+                onClick={resetInputStyle}
               />
-              <i id={styles.icons} class="las la-lock"></i>
+              <i
+                onMouseOver={showMyPassword}
+                onMouseLeave={hideMyPassword}
+                id={errorStyleIcon}
+                class="las la-lock"
+              ></i>
             </div>
-            <Btn className={styles.button} content={"Create an account"} />
+            <p className={styles.error}>{passwordError}</p>
+            <Btn
+              type={"submit"}
+              className={styles.button}
+              content={"Create an account"}
+            />
+            <h6 id={styles.h6}>
+              Already a member?{" "}
+              <span onClick={logIn} className={styles.login}>
+                Log In
+              </span>
+            </h6>
           </form>
         </div>
         <div className={styles.designcontainer}>
---
+++ src/Components/Success.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import ChangingProgressProvider from "../Utils/ChangingProgressProvider.js";
+import { buildStyles } from "react-circular-progressbar";
+import styles from "./Success.module.scss";
+
+import "react-circular-progressbar/dist/styles.css";
+import { Navigate, useNavigate } from "react-router-dom";
+
+function Success() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // Start counting when the page is loaded
+    setTimeout(() => {
+      navigate("/logIn");
+    }, 5500);
+  });
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modal_content}>
+        <div className={styles.congrats}>
+          <h2>Congratulations!</h2>
+          <h3>Your account has been successfully created.</h3>
+        </div>
+        <div className={styles.progress}>
+          <ChangingProgressProvider values={[0, 20, 40, 60, 80, 100]}>
+            {(percentage) => (
+              <CircularProgressbar
+                styles={buildStyles({
+                  pathColor: `rgb(42, 101, 61)`,
+                  textColor: "rgb(42, 101, 61)",
+                  textSize: "15px",
+                })}
+                value={percentage}
+                text={`${percentage}%`}
+              />
+            )}
+          </ChangingProgressProvider>
+          <h4>Redirecting to Log In page</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Success;
